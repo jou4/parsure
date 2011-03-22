50344d0ae592b570c95d062f6c93899b7914ea93
jou4 <jou4.kz@gmail.com>
2011-03-22 19:49:26 +0900

    modify readme

---
+++ README.md
@@ -0,0 +1,13 @@
+# parsure
+
+Text parser combinators like parsec. This is unstable because this is still developing.
+
+## Usage
+
+See examples.
+
+## License
+
+Copyright (C) 2010 K.Kamitsukasa
+
+Licensed under the MIT License (http://www.opensource.org/licenses/mit-license.php)
--- project.clj
+++ project.clj
@@ -1,6 +1,10 @@
-(defproject parsure "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
+(defproject parsure "0.0.1-SNAPSHOT"
+  :author "jou4(K.Kamitsukasa)"
+  :description "Text parser combinators like parsec."
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[vimclojure/server "2.2.0"]
-                     [org.clojars.autre/lein-vimclojure "1.0.0"]])
+                     [org.clojars.autre/lein-vimclojure "1.0.0"]]
+  :license {:name "MIT License"
+            :url "http://www.opensource.org/licenses/mit-license.php"
+            :distribution :repo})
